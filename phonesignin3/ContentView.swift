cbdb4db215ef1f13fc946778b1cf67e4d9431f25
sheehanmunim <sheehanmunim@gmail.com>
2024-08-06 13:51:10 -0400

    Initial Commit

---
+++ phonesignin3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  phonesignin3
+//
+//  Created by Sheehan Munim on 8/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ phonesignin3/phonesignin3App.swift
@@ -0,0 +1,17 @@
+//
+//  phonesignin3App.swift
+//  phonesignin3
+//
+//  Created by Sheehan Munim on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct phonesignin3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
